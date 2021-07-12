f14bf5e6801071cd6082094cff62ad19809045f2
Joseph Keable <keablejoseph@gmail.com>
2021-07-12 16:54:37 +0100

    completes video to 01:22:33

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <Nav />
+  <div class="container">
+    <Hero/>
+    <LargeCardDisplay 
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+    <SmallCardDisplay 
+      v-for="cardInfo in smallCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+  </div>
+</div>
+</template>
+
+<script>
+  import { largeCardSections } from "@/assets/data.js"
+  import { smallCardSections } from "@/assets/data.js"
+  export default {
+    data() {
+      return {
+        largeCardInfo: largeCardSections,
+        smallCardInfo: smallCardSections
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
